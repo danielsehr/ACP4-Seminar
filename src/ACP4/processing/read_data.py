234c1ed07a1cffa1b95a69c4aee385b0818b3754
danielsehr <daniel.serr7028@gmail.com>
2026-08-18 11:03:44 +0200

    new: reading functions for datasets, add: dataclass for handling all three datasets

---
+++ src/ACP4/processing/read_data.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import pandas as pd
+from dataclasses import dataclass, fields
+
+
+@dataclass
+class Data:
+    camel: pd.Series 
+    agro_herxheimweyher: pd.Series
+    agro_steinweiler: pd.Series
+
+
+def filter_camel_for_gauge_id(
+    timeseries_dir: str | Path,
+    gauge_id: str
+    ):
+    
+    return [
+        p 
+        for p in Path(timeseries_dir).rglob("*.csv") 
+        if gauge_id in p.stem.split("_")[4]
+    ][0]
+
+
+def read_camel_data(
+    timeseries_dir: str | Path,
+    gauge_id: str
+    ) -> pd.Series:
+    
+    filepath = filter_camel_for_gauge_id(
+        timeseries_dir=timeseries_dir,
+        gauge_id=gauge_id
+        )
+    
+    df = pd.read_csv(
+        filepath_or_buffer=filepath, 
+        sep=",", decimal="."
+        )
+
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.set_index("date")
+    
+    return df
+
+
+def read_agro_data(timeseries_dir: str) -> dict:
+    csv_paths = [p for p in Path(timeseries_dir).rglob("*.csv")]
+
+    agro_dict = {}
+
+    for path in csv_paths:
+        file_name = path.stem
+        df = pd.read_csv(filepath_or_buffer=path, sep=";", decimal=",")
+        
+        df = df.rename(columns={"Tag": "Date"})
+        
+        df["Date"] = pd.to_datetime(df["Date"], format="%d.%m.%Y")
+        df = df.set_index("Date")
+        
+        agro_dict[file_name] = df
+    
+    return agro_dict
