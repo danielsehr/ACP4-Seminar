60378c5b6a22f55230767e5c9a200b98173757c8
danielsehr <daniel.serr7028@gmail.com>
2026-08-18 11:04:33 +0200

    new: function to concat 6 csv agrometeo files to one, since it was only possible to donwload 2 years at once..

---
+++ src/ACP4/processing/concat_agrometeo_data.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import pandas as pd
+
+from src.ACP4.config.config import Config
+config = Config()
+
+
+paths = [
+    config.agrometeo_herxheimweyher_dir,
+    config.agrometeo_steinweiler_dir
+]
+
+def concat_agrometeo_data(
+    paths: list[str] | list[Path] = paths
+    ) -> None:
+    
+    for path in paths:
+        files = sorted(list(Path(path).glob("*.csv")))
+        
+        output_dir = Path(path)
+        output_path = output_dir / f"{output_dir.stem}_concat.csv"
+
+        df = pd.concat(
+            [pd.read_csv(file, sep=";", decimal=",") for file in files],
+            ignore_index=True,
+        )
+
+        df.to_csv(output_path, index=False)
