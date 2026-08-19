db8238358a02c194c93fb235a76e65017930bdf3
danielsehr <daniel.serr7028@gmail.com>
2026-08-19 17:25:40 +0200

    new: processing functions for renaming, and intersecting common columns and datetime

---
+++ src/ACP4/processing/process_data.py
@@ -0,0 +1,83 @@
+from dataclasses import fields
+from acp4.io.read_data import Data
+
+agro_column_mapping = {
+    "SUM_NN050": "precipitation_mean",
+    "AVG_RH200": "humidity_mean",
+    "SUM_GS200": "radiation_global_mean",
+    "AVG_TA200": "temperature_mean",
+    "MIN_TA200min": "temperature_min",
+    "MAX_TA200max": "temperature_max",
+    "SUM_PEN": "potential_evapotranspiration",
+}
+
+
+def rename_agro_data(
+    data: dict, 
+    mapping: dict = agro_column_mapping
+    ) -> dict:
+    
+    for key, df in data.items():
+        df = df.rename(columns=agro_column_mapping)
+        
+        data[key] = df
+
+    return data
+
+
+def intersect_columns(data: Data) -> Data:
+
+    common_columns = None
+
+    for field in fields(data):
+        df = getattr(data, field.name)
+        
+        if field.name == "discharge":
+            continue
+        
+        if common_columns is None:
+            common_columns = df.columns
+        else:
+            common_columns = common_columns.intersection(df.columns)
+
+    for field in fields(data):
+        df = getattr(data, field.name)
+        
+        if field.name == "discharge":
+                    continue
+        
+        setattr(
+            data,
+            field.name,
+            df.loc[:, common_columns]
+        )
+
+    return data
+    
+
+def intersect_datetime(data: Data) -> Data:
+    
+        start = max(
+            getattr(data, field.name).index.min()
+            for field in fields(data)
+        )
+        
+        end = min(
+            getattr(data, field.name).index.max()
+            for field in fields(data)
+        )
+        
+        return Data(
+            **{
+                field.name: getattr(data, field.name).loc[start:end]
+                for field in fields(data)
+            }
+        )
+
+
+def align_climate_timeseries(data: Data) -> Data:
+    
+    data = intersect_columns(data=data)
+    data = intersect_datetime(data=data)
+    
+    return data
