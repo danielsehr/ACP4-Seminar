8963a94d5a3d332e4f8d0862e2565238829bebb6
danielsehr <daniel.serr7028@gmail.com>
2026-08-19 17:21:33 +0200

    add plotting function for plotting all data and discharge only with na values

--- src/ACP4/processing/check_data.py
+++ src/ACP4/processing/check_data.py
@@ -2,26 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from dataclasses import fields
-from ACP4.processing.read_data import Data
-
-
-def align_timeseries(
-    data: Data,
-    ) -> Data:
-    
-    start = max(
-        getattr(data, field.name).index.min()
-        for field in fields(data)
-    )
-    
-    end = min(
-        getattr(data, field.name).index.max()
-        for field in fields(data)
-    )
-    
-    for field in fields(data):
-        df = getattr(data, field.name)
-        setattr(df, field.name, df.loc[start:end])
+from acp4.io.read_data import Data
 
 
 def print_data_length(data: Data) -> None:
@@ -53,8 +34,90 @@ def check_missing_values(data: Data) -> None:
 
         print("------------------------")
         
+
+def check_duplicated_dates(data: Data) -> None:
+    for field in fields(data):
+        df = getattr(data, field.name)
+
+        print(f"Name: {field.name}")
+        print(f"Duplicated dates: {df.index.duplicated().any()}")
+        print("------------------------")
+
+
+def plot_flow_precip_temp(data: Data) -> None:
+    fig, axs = plt.subplots(
+    nrows=4,
+    ncols=1,
+    figsize=(12, 8),
+    sharex=True,
+    )
+
+    # --- Discharge ---
+    axs[0].plot(
+        data.discharge.index,
+        data.discharge["discharge_spec_obs"],
+        linewidth=0.8,
+        color="black"
+    )
+
+    axs[0].set_ylabel("Discharge [m³/s]")
+    axs[0].grid(alpha=0.3)
+
+
+    # --- Temperature and Precipitation ---
+    plot_idx = 1
+
+    for field in fields(data):
+        if field.name == "discharge":
+            continue
+
+        df = getattr(data, field.name)
+
+        ax_temp = axs[plot_idx]
+        ax_precip = ax_temp.twinx()
+
+        ax_temp.plot(
+            df.index,
+            df["temperature_mean"],
+            linewidth=0.8,
+            color="red",
+        )
+
+        ax_temp.set_ylabel("Temperature [°C]")
+
+        ax_precip.bar(
+            df.index,
+            df["precipitation_mean"],
+            width=1.0,
+            alpha=0.5,
+        )
+
+        ax_precip.set_ylabel("Precipitation [mm]")
+        ax_precip.invert_yaxis()
+
+        ax_temp.set_xlabel("Date")
+        ax_temp.grid(alpha=0.3)
+
+        plot_idx += 1
+
+    fig.tight_layout()
+
+    plt.show()
         
-def plot_daily_flow(df: pd.Series) -> None:
+        
+def plot_flow_with_na(
+    df: pd.DataFrame,
+    start: str | None = None,
+    end: str | None = None,
+    ) -> None:
+    
+    if start is not None:
+            df = df[df.index >= start]
+
+    if end is not None:
+        df = df[df.index < end]
+
+
     fig, ax = plt.subplots(figsize=(10, 5))
 
     ax.plot(
@@ -62,6 +125,17 @@ def plot_daily_flow(df: pd.Series) -> None:
         df["discharge_spec_obs"],
         linewidth=1.0,
     )
+    
+    # Missing observations
+    na_mask = df["discharge_spec_obs"].isna()
+    
+    for date in df.index[na_mask]:
+        ax.axvline(
+            date,
+            color="red",
+            alpha=0.5,
+            linewidth=1.0,
+        )
 
     ax.set_title("Watershed: DEB10810, Klingbach, Herxheim")
     ax.set_xlabel("Date")
