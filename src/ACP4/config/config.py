037297baab8d9021b30938867a8ac120228dc284
danielsehr <daniel.serr7028@gmail.com>
2026-08-21 10:22:13 +0200

    add:file paths, changed agrometeo points to tuple, add solar constant

--- src/ACP4/config/config.py
+++ src/ACP4/config/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass
 class Config:
@@ -7,20 +7,21 @@ class Config:
     
     catchment_dir: str = "data/camels_de/CAMELS_DE_catchment_boundaries/catchments"
     catchment_gpkg_path: str = "data/camels_de/CAMELS_DE_catchment_boundaries/catchments/CAMELS_DE_catchments.gpkg"
+    dem_asc_path: str = "data/mapping_files/dgm200_utm32s.asc"
+    gauge_gpkg_path: str = "data/camels_de/CAMELS_DE_catchment_boundaries/gauging_stations/CAMELS_DE_gauging_stations.gpkg"
+    landcover_csv_path: str = "data/camels_de/CAMELS_DE_landcover_attributes.csv"
     
     timeseries_dir: str = "data/camels_de/timeseries"    
+    timeseries_simulated_dir: str = "data/camels_de/timeseries_simulated"    
     agrometeo_dir: str = "data/agrarmeteorologie_stations"
+    agrometeo_steinweiler_dir: str = "data/agrarmeteorologie_stations/steinweiler"
+    agrometeo_herxheimweyher_dir: str = "data/agrarmeteorologie_stations/herxheimweyher"
 
-# class Excercise1Config:
-#     catchment_dir: str = "data/camels_de/CAMELS_DE_catchment_boundaries/catchments"
-#     catchment_gpkg_path: str = "data/camels_de/CAMELS_DE_catchment_boundaries/catchments/CAMELS_DE_catchments.gpkg"
-#     fid: int = 1193
-#     gauge_id: str = "DEB10810"
+    agrometeo_points: tuple = (("Herxheimweyher", 8.25, 49.16),
+                               ("Steinweiler", 8.10, 49.10))
+
+    solar_constant: float = 0.0820 #  MJ m-2 min-1
     
-#     agrometeo_dir: str = "data/agrarmeteorologie_stations"
-#     herxheimweyher_lon_lat: tuple = ("Herxheimweyher", 8.25, 49.16)
-#     steinweiler_lon_lat: tuple = ("Steinweiler", 8.10, 49.10)
-
 
 # @dataclass
 # class Excercise2Config:
